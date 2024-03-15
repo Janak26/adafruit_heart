781c3d4e53bdcd6cbfb69ac54904517df6b52913
Janak26 <janakrathod26@gmail.com>
2024-03-15 18:42:45 -0400

    redo

--- Adafruit_GPIO/Platform.py
+++ Adafruit_GPIO/Platform.py
@@ -66,20 +66,19 @@ def pi_revision():
     """Detect the revision number of a Raspberry Pi, useful for changing
     functionality like default I2C bus based on revision."""
     # Revision list available at: http://elinux.org/RPi_HardwareHistory#Board_Revision_History
-    # with open('/proc/cpuinfo', 'r') as infile:
-    infile = ["Revision : 0002"]
-    for line in infile:
-        # Match a line of the form "Revision : 0002" while ignoring extra
-        # info in front of the revsion (like 1000 when the Pi was over-volted).
-        match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
-        if match and match.group(1) in ['0000', '0002', '0003']:
-            # Return revision 1 if revision ends with 0000, 0002 or 0003.
-            return 1
-        elif match:
-            # Assume revision 2 if revision ends with any other 4 chars.
-            return 2
-    # Couldn't find the revision, throw an exception.
-    raise RuntimeError('Could not determine Raspberry Pi revision.')
+    with open('/proc/cpuinfo', 'r') as infile:
+        for line in infile:
+            # Match a line of the form "Revision : 0002" while ignoring extra
+            # info in front of the revsion (like 1000 when the Pi was over-volted).
+            match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
+            if match and match.group(1) in ['0000', '0002', '0003']:
+                # Return revision 1 if revision ends with 0000, 0002 or 0003.
+                return 1
+            elif match:
+                # Assume revision 2 if revision ends with any other 4 chars.
+                return 2
+        # Couldn't find the revision, throw an exception.
+        raise RuntimeError('Could not determine Raspberry Pi revision.')
 
 
 def pi_version():
@@ -92,10 +91,9 @@ def pi_version():
     # 2709 is pi 2
     # 2835 is pi 3 on 4.9.x kernel
     # Anything else is not a pi.
-    # with open('/proc/cpuinfo', 'r') as infile:
-        # cpuinfo = infile.read()
+    with open('/proc/cpuinfo', 'r') as infile:
+        cpuinfo = infile.read()
     # Match a line like 'Hardware   : BCM2709'
-    cpuinfo = "Hardware   : BCM2709"
     match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo,
                       flags=re.MULTILINE | re.IGNORECASE)
     if not match:
